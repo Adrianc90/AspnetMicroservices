6ca22f09c1c6a643910cce3ac7b9d0ebbc4d944e
Adrianc90 <adco0211@gmail.com>
2021-04-28 18:43:36 -0500

    Adding Controllers

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,18 @@
+using Catalog.API.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public CatalogContext(IConfiguration conf)
+        {
+            var client = new MongoClient(conf.GetValue<string>("DatabaseSettings:ConnectionString"));
+            var database = client.GetDatabase(conf.GetValue<string>("DatabaseSettings:DatabaseName"));
+            Products = database.GetCollection<Product>(conf.GetValue<string>("DatabaseSettings:CollectionName"));
+            CatalogContextSeed.SeedData(Products);
+        }
+        public IMongoCollection<Product> Products { get; }
+    }
+}
